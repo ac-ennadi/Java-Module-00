5396a58de1e5552f6dc819094160010cc38ac6b7
ac-ennadi <achrafennadiri@gmail.com>
2026-09-20 23:15:00 +0000

    add a testfile

---
+++ test/main.java
@@ -0,0 +1,7 @@
+public class Main {
+    static public void main(String[] arg)
+    {
+        for (int i = 0; i < arg.length; i++)
+            System.out.println("The output arg[" + i + "]: " + arg[i]);
+    }
+}
