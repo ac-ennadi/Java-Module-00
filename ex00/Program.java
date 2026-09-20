de11b9cae08986ec006ab9d57fb1d836fddf4dd3
ac-ennadi <achrafennadiri@gmail.com>
2026-09-20 23:15:00 +0000

    finish exercice 1

--- ex00/Program.java
+++ ex00/Program.java
@@ -1,6 +1,12 @@
 public class Program {
+
+    public static int add(int a, int b, int c, int d, int e, int f)
+    {
+        return (a + b + c + d + e + f);
+    }
     public static void main(String[] arr)
     {
-        System.out.println("hello world");
+        int sum = add(4,7,9,5,9,8);
+        System.out.println(sum);
     }
 }
